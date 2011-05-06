002103b98ec0c0b980771275f73154acb91676c3
tjcjc <taijcjc@gmail.com>
2011-05-07 05:41:21 +0800

    ok

---
+++ lib/generators/multi_hmab/initializer_generator.rb
@@ -0,0 +1,27 @@
+module MultiHmab
+  module Generators
+    class InitializerGererator < ActiveRecord::Generators::NamedBase
+      source_root File.expand_path("../templates", __FILE__)
+
+      def generate_model_and_migration
+        multi_end, single_end = singular_table_name.split("_")
+        run_ruby_script("rails g model #{class_name} #{multi_end}:references #{single_end}:references relation_type:integer")
+      end
+
+      #def generate_model
+        #multi_end, single_end = singular_table_name.split("_")
+        #create_file "app/models/#{singular_table_name}.rb", <<-CONTENT
+          #class #{class_name}
+            #belongs_to :#{multi_end}
+            #belongs_to :#{single_end}
+          #end
+        #CONTENT
+      #end
+
+      #def generate_migration
+        #multi_end, single_end = singular_table_name.split("_")
+        #migration_template 'migration.rb', "db/migrate/create_#{table_name}.rb", :multi_end => multi_end, :single_end => single_end
+      #end
+    end
+  end
+end
